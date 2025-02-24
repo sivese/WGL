f93650d013a5276104cf76bfd0a1c990d9e321b7
sivese <driversivese@gmail.com>
2025-02-24 09:38:47 +0900

    update triangle

--- src/wgl.js
+++ src/wgl.js
@@ -1,12 +1,14 @@
 
 //vertex
 async function load_vshader_source() {
-    fetch('/shader/vshader.glsl');
+    let res = await fetch('/shader/vshader.glsl');
+    return res.text();
 }
 
 //fragment
 async function load_fshader_source() {
-    fetch('/shader/fshader.glsl');
+    let res = await fetch('/shader/fshader.glsl');
+    return res.text();
 }
 
 // gl -> gl context!
@@ -26,6 +28,24 @@ function create_shader(gl, shader_type, shader_source) {
     gl.deleteShader(shader);
 }
 
+// v -> vertex, f -> fragment
+function create_program(gl, vshader, fshader) {
+    let program = gl.createProgram();
+    
+    gl.attachShader(program, vshader);
+    gl.attachShader(program, fshader);
+    gl.linkProgram(program);
+
+    let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    
+    if(success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
 async function main() {
     let canvas = document.querySelector("#c");
     let gl = canvas.getContext("webgl2");
@@ -35,8 +55,60 @@ async function main() {
         return;
     }
 
-    var vshader_source = await load_vshader_source();
-    var fshader_source = await load_fshader_source();
+    let vshader_source = await load_vshader_source();
+    let vshader = create_shader(gl, gl.VERTEX_SHADER, vshader_source);
+
+    let fshader_source = await load_fshader_source();
+    let fshader = create_shader(gl, gl.FRAGMENT_SHADER, fshader_source);
+
+    let gl_program = create_program(gl, vshader, fshader);
+
+    let position_attribute_location = gl.getAttribLocation(gl_program, "a_position");
+    let position_buffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
+
+    let positions = [
+        0, 0,
+        0, 0.5,
+        0.7, 0,
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    
+    //VAO = vertext array object
+    let vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+    gl.enableVertexAttribArray(position_attribute_location);
+
+    // vertext attribute pointer parameters
+    let size = 2;
+    let type = gl.FLOAT;
+    let normalize = false;
+    let stride = 0;
+    let offset = 0;
+
+    gl.vertexAttribPointer(
+        position_attribute_location, size, type, normalize, stride, offset
+    );
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(gl_program);
+
+    // Bind the attribute/buffer set we want.
+    gl.bindVertexArray(vao);
+
+    let primitiveType = gl.TRIANGLES;
+    let count = 3;
+    offset = 0; //variable reuse
+    
+    gl.drawArrays(primitiveType, offset, count);
 }
 
 main();
